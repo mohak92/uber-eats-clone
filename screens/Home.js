95899a0906a09549ea183f4b9b1a24ad401ef091
mohak92 <tamhane.mohak@gmail.com>
2022-06-14 07:53:12 -0400

    add components

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, SafeAreaView } from "react-native"
 import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
+import Categories from "../components/Categories";
 
 export default function Home() {
   return (
     <SafeAreaView style={{backgroundColor: "#eee", flex: 1}}>
       <View style={{backgroundColor: "white", padding: 15}}>
         <HeaderTabs />
+        <SearchBar />
       </View>
+      <Categories />
     </SafeAreaView>
   );
 }
