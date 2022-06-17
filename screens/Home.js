df0503ec8b69ac0fe602f6b586324e181cbb1fe6
mohak92 <tamhane.mohak@gmail.com>
2022-06-16 20:47:28 -0400

    add restaurant item component

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,47 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, SafeAreaView } from "react-native"
+import { Text, View, SafeAreaView, ScrollView } from "react-native";
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "../components/SearchBar";
 import Categories from "../components/Categories";
+import RestaurantItems, {
+  localRestaurants,
+} from "../components/RestaurantItems";
+
+const YELP_API_KEY =
+  "";
 
 export default function Home() {
+  const [restaurantData, setRestaurantData] = React.useState(localRestaurants);
+
+  const getRestaurantsFromYelp = () => {
+    const yelpUrl =
+      "https://api.yelp.com/v3/businesses/search?term=restaurants&location=Princeton";
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => res.json())
+      .then((json) => setRestaurantData(json.businesses));
+  };
+
+  useEffect(() => {
+    getRestaurantsFromYelp();
+  }, [])
+
   return (
-    <SafeAreaView style={{backgroundColor: "#eee", flex: 1}}>
-      <View style={{backgroundColor: "white", padding: 15}}>
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
         <HeaderTabs />
         <SearchBar />
       </View>
-      <Categories />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItems restaurantData={restaurantData} />
+      </ScrollView>
     </SafeAreaView>
   );
 }
